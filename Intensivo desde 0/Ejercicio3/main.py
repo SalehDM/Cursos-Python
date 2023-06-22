9cd8a3bb9f7419f94716e273d26a45d446066ad6
SalehDM <136188886+SalehDM@users.noreply.github.com>
2023-06-22 21:54:59 +0200

    Add files via upload

---
+++ Intensivo desde 0/Ejercicio3/main.py
@@ -0,0 +1,27 @@
+import os
+
+def primos_o_no (num):
+    iterador=num
+
+    while iterador>2:
+        iterador-=1
+        if num % iterador == 0:
+            return False
+            break
+    return True
+
+def main():
+    os.system('cls')
+    print('Calcular números primos')
+    print('Introduzca un número entero mayor que 1')
+    num=int(input())
+    while num<1:
+        print('Número incorrecto, introduzca un número entero mayor que 1')
+        num=int(input())
+
+    primos=list(filter(primos_o_no,list(range(num,1,-1))))
+
+    print(f'Los números primos entre el {num} y el 1 son {", ".join(map(str, primos))}')
+
+if __name__=='__main__':
+    main()
