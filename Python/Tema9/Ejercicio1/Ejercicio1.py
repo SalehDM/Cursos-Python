27fd8c627a85005a65821b4208ac8f82916ea4cd
SalehDM <136188886+SalehDM@users.noreply.github.com>
2023-07-11 23:53:51 +0200

    Ejercicio1.py

--- Python/Tema9/Ejercicio1/Ejercicio1.py
+++ Python/Tema9/Ejercicio1/Ejercicio1.py
@@ -1,3 +1,7 @@
+"""
+Enunciado del ejercicio:
+    Crea un script que le pida al usuario una lista de países (separados por comas). Éstos se deben almacenar en una lista. No debería haber países repetidos (haz uso de set). Finalmente, muestra por consola la lista de países ordenados alfabéticamente y separados por comas.
+"""
 def main():
     paises=input("Introduce una lista de paises separados por comas:\n")
     lpaises=set(paises.lower().title().split(","))
