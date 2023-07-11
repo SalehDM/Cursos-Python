9c53fe6d990bea511a7eb072dca39708ee904f07
SalehDM <136188886+SalehDM@users.noreply.github.com>
2023-07-11 23:36:22 +0200

    main.py

--- Intensivo desde 0/Ejercicio3/main.py
+++ Intensivo desde 0/Ejercicio3/main.py
@@ -1,3 +1,10 @@
+"""
+Ejercicio 3
+Enunciado: Crea una función que calcule los números primos entre 1 y N, siendo N el parámetro que recibe la función.
+Objetivo:
+    Uso de bucles anidados
+    El uso de colecciones
+"""
 import os
 
 def primos_o_no (num):
