5ba5481f473393e3112c017ac7a6484814766c6e
SalehDM <136188886+SalehDM@users.noreply.github.com>
2023-07-11 23:48:01 +0200

    main.py

--- Python/Tema11/Ejercicio1/main.py
+++ Python/Tema11/Ejercicio1/main.py
@@ -1,3 +1,9 @@
+"""
+Enunciado del ejercicio:
+    En este ejercicio tendréis que crear una tabla llamada Alumnos que constará de tres columnas: la columna id de tipo entero, la columna nombre que será de tipo texto y la columna apellido que también será de tipo texto.
+    Una vez creada la tabla, tenéis que insertarle datos, como mínimo tenéis que insertar 8 alumnos a la tabla.
+    Por último, tienes que realizar una búsqueda de un alumno por nombre y mostrar los datos por consola.
+"""
 import sqlite3
 
 conn=sqlite3.connect('C:/Users/PC/Desktop/Python/Python/Tema11/Ejercicio1/mydbtema11.db')
