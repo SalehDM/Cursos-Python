0a0c32487b5f343ac64851af6c50d426e49a89bf
SalehDM <136188886+SalehDM@users.noreply.github.com>
2023-07-11 23:37:23 +0200

    main.py

--- Intensivo desde 0/Ejercicio4/main.py
+++ Intensivo desde 0/Ejercicio4/main.py
@@ -1,3 +1,10 @@
+"""
+Ejercicio 4
+Enunciado: Utilizando la API de https://openweathermap.org/ y realizando una petición a https://api.openweathermap.org/data/2.5/weather?q={city%20name}&appid={API%20key}, obtén la temperatura máxima y mínima, para la ciudad que proporcione el usuario.
+Objetivo:
+    Aprender a utilizar librerías externas (en este caso, requests)
+    Manipular el resultado de la petición (JSON)
+"""
 import requests
 import json
 import os
