053de70f32d59572e6eedb62ca63e8b1bb747a9c
SalehDM <136188886+SalehDM@users.noreply.github.com>
2023-07-11 23:52:21 +0200

    main.py

--- Python/Tema10/Ejercicio2/main.py
+++ Python/Tema10/Ejercicio2/main.py
@@ -1,3 +1,7 @@
+"""
+Enunciado del ejercicio:
+    En este segundo ejercicio, tendréis que crear una interfaz sencilla la cual debe de contener una lista de elementos seleccionables, también debe de tener un label con el texto que queráis.
+"""
 import tkinter as tk
 from tkinter import ttk
 
