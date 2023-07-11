b287471910cc0125bc231bc927fd8d7f45721649
SalehDM <136188886+SalehDM@users.noreply.github.com>
2023-07-11 23:56:24 +0200

    main.py

--- Python/Tema8/Ejercicio1/main.py
+++ Python/Tema8/Ejercicio1/main.py
@@ -1,3 +1,7 @@
+"""
+Enunciado del ejercicio:
+    En este ejercicio, tendréis que crear un archivo py donde creéis un archivo txt, lo abráis y escribáis dentro del archivo. Para ello, tendréis que acceder dos veces al archivo creado.
+"""
 def main():
     f=open('C:/Users/PC/Desktop/Python/Python/Tema8/Ejercicio1/archivo.txt','wt')
     f.close()
