b4ac0e73fed2b005736b7fe5fc1a839aaa58afb7
SalehDM <136188886+SalehDM@users.noreply.github.com>
2023-07-11 23:44:55 +0200

    manage.py

--- Python/Tema12/Ejercicio1/manage.py
+++ Python/Tema12/Ejercicio1/manage.py
@@ -1,3 +1,10 @@
+"""
+Enunciado del ejercicio:
+    En este ejercicio tendrás que crear una aplicación en Django que almacene datos de directores de cine y luego se puedan ver sus películas, así como una descripción de las mismas.
+    Tienes que personalizar el admin de la aplicación y a su vez crear las vistas de cada una de las partes de la aplicación.
+        Usuario:SuperAdmin
+        Contraseña:Super12345
+"""
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
