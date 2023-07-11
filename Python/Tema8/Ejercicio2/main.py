7b27904a6f6fc2e0650b466ee0f4256166f5d834
SalehDM <136188886+SalehDM@users.noreply.github.com>
2023-07-11 23:57:31 +0200

    main.py

--- Python/Tema8/Ejercicio2/main.py
+++ Python/Tema8/Ejercicio2/main.py
@@ -1,3 +1,7 @@
+"""
+Enunciado del ejercicio:
+    En este segundo ejercicio, tendréis que crear un archivo py y dentro crearéis una clase Vehículo, haréis un objeto de ella, lo guardaréis en un archivo y luego lo cargamos.
+"""
 import pickle
 import vehiculo as v
 
